a8b91c0aa944f5a5075f41c3651e40bc846a4e26
baek0101 <bjh010137@gmail.com>
2020-06-22 23:55:01 +0900

    while문 문제

---
+++ exam/200622/src/Ex1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Ex1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bW = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		while(true) {
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			bw.Writer.
+		}
+
+	}
+
+}
