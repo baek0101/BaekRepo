df2a089bb2025000db4682686f4a797b02f1a308
baek0101 <bjh010137@gmail.com>
2020-06-23 13:15:53 +0900

    while문 문제

---
+++ exam/200623/src/Ex2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Ex2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner s = new Scanner(System.in);
+		
+		while(s.hasNext()) {
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(a+b);
+		}
+	}
+	
+}
