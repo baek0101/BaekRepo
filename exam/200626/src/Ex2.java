55089ab3f669e6fff2114dc9937849c2940c1fc0
baek0101 <bjh010137@gmail.com>
2020-06-26 23:51:25 +0900

    가장 싼 세트 메뉴의 가격을 출력하는 프로그램

---
+++ exam/200626/src/Ex2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int aa[] = new int[5];
+		
+		int i = 0;
+		while(i < aa.length) {
+			aa[i] = Integer.parseInt(br.readLine());
+		}
+		
+		if(aa[0] >= aa[1]) {
+			if(aa[0] <= aa[3]) {
+				
+			}
+		}
+
+		
+	}
+
+}
